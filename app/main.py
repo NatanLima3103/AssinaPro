59406e08a8d22f1fed91e39dfe2d32374c9775cd
Natan Lima <natanjonatan3103@gmail.com>
2026-04-10 16:22:44 -0300

    Notebook corporativo (Configuração inicial do projeto com FastAPI, templates Jinja2 e arquivos estáticos; criação da página inicial com formulário profissional para geração de assinatura e base visual em CSS.)

--- app/main.py
+++ app/main.py
@@ -1,1 +1,11 @@
-#somente teste...
+from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
+templates = Jinja2Templates(directory="app/templates")
+
+@app.get("/")
+async def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
